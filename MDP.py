cea8c54e26b1e27cfc74470f2d1c02213f0705e6
yanyunfeng <yy3034@columbia.edu>
2020-04-24 01:36:37 +0800

    Enable inserting features into states

--- MDP.py
+++ MDP.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 
 ######################################
@@ -11,29 +12,37 @@ class TradeMDP:
     # timeGap: the number
     #  of ticks between two operations (Const)
     # states: the whole set of states the MDP could have
-    # T_states: the states divided by the time level
+    # T_states: the states grouped by the time level
     # VolState: 0:None; 1:1-eachVol; 2:eachVol+1 - 2eachVol ...
-    def __init__(self, totalVol, voLevel, timeLevel, priceFlex, timeGap):
+    def __init__(self, totalVol, voLevel, timeLevel, priceFlex, timeGap, FeatureLevel, FeatureNum):
+        # 目前暂时把FeatureLevel统一设一个数字
+
         self.totalVol = totalVol
         self.voLevel = voLevel
         self.timeLevel = timeLevel
         self.priceFlex = priceFlex
         self.timeGap = timeGap
         self.eachVol = totalVol // voLevel
-        self.states = []
-        self.T_states = []
-        for i in range(self.timeLevel + 1):
-            T_state = []
-            for j in range(1, self.voLevel + 1):
-                self.states.append((i, j))
-                T_state.append((i, j))
-            self.T_states.append(T_state)
+        self.FeatureNum = FeatureNum
+        self.FeatureLevel = FeatureLevel
+
+        def list2tuple(l):
+            return tuple(l)
+
+        timeLevel = [x for x in range(self.timeLevel + 1)]
+        voLevel = [x for x in range(1, self.voLevel + 1)]
+        featLevel = [x for x in range(1, self.FeatureLevel + 1)]
+        mesh_input = [timeLevel, voLevel] + [featLevel] * FeatureNum
+        # 有几个feature, 就要在meshgrid里放几个featLevel
+        temp = np.array(np.meshgrid(*mesh_input)).T.reshape(-1, 2 + FeatureNum)
+        self.states = list(map(list2tuple, temp))
 
         # For terminal state, when the remain is 0, the state is None
         self.states.append(None)
 
     def startState(self):
-        digit = (0, self.totalVol)
+        digit = [0, self.totalVol] + [self.FeatureLevel] * self.FeatureNum
+        digit = tuple(digit)
         return self.JudgeState(digit)
 
     # Return set of actions possible from |state|.
@@ -47,14 +56,16 @@ class TradeMDP:
         elif state[0] == self.timeLevel:
             return ['MO']
         else:
-            return [str(i) for i in range(-self.priceFlex, self.priceFlex+1)]
+            return [str(i) for i in range(-self.priceFlex, self.priceFlex + 1)]
 
     # Given the accurate digit, we decide the state it is in
     def JudgeState(self, digit):
-        if int(digit[1]) == 0:
+        # 这个版本可以兼容state里有任意数量的feature
+        volState = int(((digit[1] - 1) // self.eachVol) + 1)
+        if digit[1] == 0 or volState == 0:
             return
-        volState = ((digit[1]-1) // self.eachVol) + 1
-        return digit[0], int(volState)
+        new_digit = tuple([digit[0], int(volState)] + list(digit[2:]))
+        return new_digit
 
 
 ############################################################
@@ -100,9 +111,10 @@ class QLearningAlgorithm:
     # We will call this function with (s, a, r, s'), update the Q-Grid and number of each state being visited
     # Note that if s is a terminal state, then s' will be None.
     def updateQ(self, state, action, reward, newState):
-        sample = max([self.getQ(newState, next_act) for next_act in self.mdp.Actions(newState)])\
+        sample = max([self.getQ(newState, next_act) for next_act in self.mdp.Actions(newState)]) \
                  + reward - self.getQ(state, action)
         self.numIters[(state, action)] += 1
+
         self.QGrid[(state, action)] += self.getStepSize(state, action) * sample
 
     def update_prob(self):
