02465b99eff71003ed4d960ead41d9ca766b139a
WJDeng <deng9610@163.com>
2020-05-03 12:28:51 +0800

    RL_training_set

--- RL.py
+++ RL.py
@@ -17,7 +17,7 @@ def Simulate(rl, orderbooks_df, quantile_df, numTrial=50, learn=True):
             Order = util.SellOrder(remain, action, orderbook)
         else:
             spread = orderbook.get_spread()
-            price = orderbook.ask_prices[0] - round(spread / 4 * int(action), 2)
+            price = orderbook.ask_prices[0] - round(spread / rl.mdp.priceFlex * int(action), 2)
             Order = util.SellOrder(remain, price, orderbook)
         return Order
 
@@ -64,14 +64,13 @@ def Simulate(rl, orderbooks_df, quantile_df, numTrial=50, learn=True):
                 Sects = [util.OrderBook(row=orderbooks[-1])]
                 future_df = None
             else:
-                ob_data = orderbooks[i * rl.mdp.timeGap:  (i + 1) * rl.mdp.timeGap]
+                ob_data = orderbooks[i * rl.mdp.timeGap:  (i+1) * rl.mdp.timeGap]
                 Sects = [util.OrderBook(row=ob) for ob in ob_data]
-                future_df = orderbooks_df.iloc[(i+1)*rl.mdp.timeGap + 1]
+                future_df = orderbooks_df.iloc[(i+1) * rl.mdp.timeGap]
 
             action = rl.getAction(state, learn)
             reward, newState, remain = ActEnv(rl, iter=i, volume=remain, action=action, orderbooks=Sects,
                                               future_df=future_df, quantile_df=quantile_df)
-
             sequences.append((state, action, reward, newState))
             state = newState
 
@@ -91,62 +90,68 @@ def Simulate(rl, orderbooks_df, quantile_df, numTrial=50, learn=True):
     rl.update_prob()
 
 
-def main(numTrial):
+def data_prepare(symbol, total_time):
+    # orderbooks only appears here
+    raw_orderbooks = pd.read_csv('./ob_{}.csv'.format(symbol))
+    lag = 50
+    f = all_features(raw_orderbooks, lag=lag)
+    orderbooks_df = pd.concat([raw_orderbooks, f], axis=1).iloc[(lag - 1):]
+    orderbooks_df.index = range(len(orderbooks_df))
+
+    # Train test split
+    num_epochs = len(orderbooks_df) // total_time
+    train_num_epochs = int(num_epochs * 0.9)
+    train_df = orderbooks_df.loc[:train_num_epochs*total_time]
+    test_df = orderbooks_df.loc[train_num_epochs*total_time:]
+    test_df.index = range(len(test_df))
+
+    # Get quantile only from train_df
+    quantile = get_quantile(train_df[['vol']], [0.33, 0.66])
+    return train_df, test_df, quantile
+
+
+def df_simulate(numTrial, q_algo, df, total_time, quantile, train=False):
+    def spliter(df, total_time, num):
+        return df.loc[num * total_time: (num + 1) * total_time]
+
+    RL_reward = []
+    SL_reward = []
+    num_epochs = len(df) // total_time
+    for i in range(num_epochs):
+        ob_piece = spliter(df, total_time, i)
+        if train:
+            Simulate(q_algo, ob_piece, quantile, numTrial=numTrial, learn=True)
+        RL_reward.append(Simulate(q_algo, ob_piece, quantile, numTrial=1, learn=False))
+        SL_reward.append(SL_Policy(q_algo.mdp.totalVol, ob_piece))
+    return np.array(RL_reward), np.array(SL_reward)
+
+
+def main(symbol, numTrial):
     # Usage: Currently, we cannot take too much features
     # If one wishes to run directly, we only apply 'volatility' and 'order_flow' in features.
-    data_dir = './'
+    totalVol = 50
+    voLevel = 5
     timeGap = 10
+    priceFlex = 4
     timeLevel = 5
 
     # First timeLevel * timeGap time: time for limit order
     # Last 1 : time for market order
     total_time = timeLevel * timeGap + 1
-
-    # orderbooks only appears here
-    raw_orderbooks = pd.read_csv(data_dir + 'orderbook_new.csv')
-    lag = 50
-    f = all_features(raw_orderbooks, lag=lag)
-    orderbooks_df = pd.concat([raw_orderbooks, f], axis=1).iloc[(lag-1):]
-    orderbooks_df.index = range(len(orderbooks_df))
-
-    quantile = get_quantile(f[['vol']], [0.33, 0.66])
+    train_df, test_df, quantile = data_prepare(symbol, total_time)
     FeatureNum = len(quantile.columns)
 
-    # Split train and test epochs
-    num_epochs = len(orderbooks_df) // total_time
-    train_epochs_num = int(num_epochs * 0.9)
-
     # Initilize MDP
-    mdp = MDP.TradeMDP(totalVol=100, voLevel=5, timeLevel=timeLevel, priceFlex=4, timeGap=timeGap, FeatureLevel=3,
-                       FeatureNum=FeatureNum)
-    q_algo = MDP.QLearningAlgorithm(mdp, exploreStep=200, init_prob=0.8, final_prob=0.1)
+    mdp = MDP.TradeMDP(totalVol=totalVol, voLevel=voLevel, timeLevel=timeLevel, priceFlex=priceFlex, timeGap=timeGap,
+                       FeatureLevel=3, FeatureNum=FeatureNum)
+    q_algo = MDP.QLearningAlgorithm(mdp, exploreStep=2000, init_prob=0.8, final_prob=0.1)
+    RL_ontrain_reward, SL_ontrain_reward = df_simulate(numTrial, q_algo, train_df, total_time, quantile, train=True)
+    RL_train_reward, SL_train_reward = df_simulate(1, q_algo, train_df, total_time, quantile, train=False)
+    RL_test_reward, SL_test_reward = df_simulate(1, q_algo, test_df, total_time, quantile, train=False)
 
-    def spliter(df, total_time, num):
-        return df.loc[num * total_time: (i + 1) * total_time]
-
-    # Training
-    RL_train_reward = []
-    SL_train_reward = []
-    for i in range(train_epochs_num):
-        ob_piece = spliter(orderbooks_df, total_time, i)
-        Simulate(q_algo, ob_piece, quantile, numTrial=numTrial, learn=True)
-        RL_train_reward.append(Simulate(q_algo, ob_piece, quantile, numTrial=1, learn=False))
-        SL_train_reward.append(SL_Policy(50, ob_piece))
-
-    # Test
-    RL_test_reward = []
-    SL_test_reward = []
-    for i in range(train_epochs_num, num_epochs):
-        ob_piece = spliter(orderbooks_df, total_time, i)
-        RL_test_reward.append(Simulate(q_algo, ob_piece, quantile, numTrial=1, learn=False))
-        SL_test_reward.append(SL_Policy(50, ob_piece))
-
-    RL_train_reward = np.array(RL_train_reward)
-    RL_test_reward = np.array(RL_test_reward)
-    SL_train_reward = np.array(SL_train_reward)
-    SL_test_reward = np.array(SL_test_reward)
-    return RL_train_reward, RL_test_reward, SL_train_reward, SL_test_reward
-
+    return RL_ontrain_reward, RL_train_reward, RL_test_reward, \
+           SL_ontrain_reward, SL_train_reward, SL_test_reward, q_algo
 
 if __name__ == '__main__':
-    RL_train_reward, RL_test_reward, SL_train_reward, SL_test_reward = main(numTrial=10)
+    RL_ontrain_reward, RL_train_reward, RL_test_reward, \
+    SL_ontrain_reward, SL_train_reward, SL_test_reward, q_algo = main(symbol='INTC', numTrial=100)
